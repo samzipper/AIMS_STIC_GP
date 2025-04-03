ea801732916133b21d8e254238b20099613a8bc6
Sam Zipper <samzipper@ku.edu>
2025-04-03 10:08:29 -0500

    Add elevation and limestone units to watershed map figure

--- figures+tables/Figure6_MapPrcWet.R
+++ figures+tables/Figure6_MapPrcWet.R
@@ -2,6 +2,8 @@
 
 ## prep workspace
 source(file.path("code", "paths+packages.R"))
+library(terra)
+library(tidyterra)
 library(readxl)
 library(ggspatial)
 
@@ -53,19 +55,48 @@ df_stic_2023 <-
             n_days = n()) |> 
   subset(n_days > 365*0.8)
 
+# load other data - watershed boundary, DEM, limestone layers
+sf_watershed <- st_read(file.path("data", "Konza_Watershed.shp"))
+r_dem <- rast(file.path("data", "Konza_DEM_m.tif")) |> 
+  mask(sf_watershed)
+df_geology <- read_csv(file.path("data", "Konza_geology.csv")) |> 
+  subset(type == "Ls") |> 
+  subset(unit %in% c("Morrill Ls", "Crouse Ls"))
+
+# in the DEM, set 1 for any value that is between the bottom_elevation_m and 
+# top_elevation_m for any row in df_geology and 0 if it is not 
+r_limestone <- r_dem
+r_limestone[is.finite(r_limestone)] <- 0
+
+for (i in 1:nrow(df_geology)) {
+  r_limestone <- 
+    r_limestone + 
+    (r_dem >= df_geology$bottom_elevation_m[i] & 
+        r_dem <= df_geology$top_elevation_m[i])
+}
+plot(r_limestone)
+
 ## map - overall
 p_overall <-
   ggplot() + 
-  geom_sf(data = sf_streams, color = col.cat.blu) +
+  geom_spatraster(data = r_dem, na.rm = T) +
+  scale_fill_viridis_c(name = "Elevation [masl]", na.value = "transparent", option = "G") +
+  geom_spatraster(data = r_limestone, aes(alpha = after_stat(value == 1)), fill = "white") +
+  scale_alpha_manual(values = c(0, 1), guide = "none") +  # 50% transparent gray where r_limestone == 1
+  #geom_sf(data = sf_watershed, color = col.gray, fill = NA) +
+  geom_sf(data = sf_streams, color = col.gray) +
   geom_sf(data = left_join(sf_sites, df_stic_overall, by = "siteId"), 
-          aes(fill = mean_prc_wet), shape = 21, size = 2) +
-  scale_fill_viridis_c(name = "% of Time Wet",
-                       limits = c(0, 1),
-                       labels = scales::percent,
-                       option = "C",
-                       direction = -1,
-                       na.value = "white") +
+          aes(color = mean_prc_wet), size = 2) +
+  scale_color_viridis_c(name = "% of Time Wet",
+                        limits = c(0, 1),
+                        labels = c("0", "25", "50", "75", "100"),
+                        option = "C",
+                        direction = -1,
+                        na.value = "white") +
   annotation_scale(location = "bl") +
+  annotation_north_arrow(location = "br", 
+                         which_north = "true", 
+                         style = north_arrow_orienteering) +
   theme(panel.border = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -79,10 +110,26 @@ p_overall <-
 ggsave(file.path("figures+tables", "Figure6_MapPrcWet-Overall.png"),
        (p_overall + 
           theme(plot.title = element_blank(),
-                #legend.position = "bottom",
+                # remove margin around plot and panel
+                plot.margin = margin(0, 0, 0, 0),
+                legend.position = "bottom",
                 legend.key.width = unit(8, "mm"),
-                legend.title = element_text(hjust = 0.5))),
-       width = 95, height = 95, units = "mm")
+                legend.title = element_text(hjust = 0.5)) +
+         guides(color = guide_colorbar(order = 1),
+                fill = guide_colorbar(order = 2))),
+       width = 95, height = 105, units = "mm")
+
+ggsave(file.path("figures+tables", "Figure6_MapPrcWet-Overall.pdf"),
+       (p_overall + 
+          theme(plot.title = element_blank(),
+                # remove margin around plot and panel
+                plot.margin = margin(0, 0, 0, 0),
+                legend.position = "bottom",
+                legend.key.width = unit(8, "mm"),
+                legend.title = element_text(hjust = 0.5)) +
+          guides(color = guide_colorbar(order = 1),
+                 fill = guide_colorbar(order = 2))),
+       width = 95, height = 105, units = "mm", device = cairo_pdf)
 
 p_2021 <-
   ggplot() + 
